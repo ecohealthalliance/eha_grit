a4417eafb96aa894620fad0f17d3530c04024d63
Zack Krejci <zack.krejci@gmail.com>
2013-03-05 12:00:13 -0500

    svm experiments

---
+++ svm/classify.py
@@ -0,0 +1,91 @@
+from sys import argv 
+import math
+from svmutil import svm_problem, svm_parameter, svm_train, svm_predict
+
+from io import read_table
+
+max_label = 0
+labels = {}
+inv_labels = {}
+
+correct = {}
+wrong = {}
+
+def label2int (label):
+    global max_label, labels, inv_labels, correct, wrong
+    if not labels.has_key (label):
+        labels[label] = max_label
+        inv_labels[max_label] = label
+        max_label += 1
+        correct[label] = 0
+        wrong[label] = 0
+    return labels[label]
+
+def int2label (label_int):
+    global max_label, labels, inv_labels
+    return inv_labels[label_int]
+
+'''def name2label (label, pos_name):
+    if label == pos_name:
+        return 1
+    else:
+        return -1'''
+
+if __name__ == '__main__':
+    data = read_table (argv[1])
+    #test = read_table (argv[2])
+
+    kernel = int (argv[2])
+
+    for i in range (0, len (data)):
+        train = []
+        test = []
+        for j, item in enumerate (data):
+            if i != j:
+                train.append (item)
+            else:
+                test.append (item)
+
+        y = []
+        x = []
+        for item in train:
+            #y.append (name2label (item['attr']['Disease'], label_name))
+            y.append (label2int (item['attr']['Disease']))
+            x.append (item['pos'])
+
+        prob = svm_problem (y, x)
+        param = svm_parameter ('-q -t %d' % (kernel,))
+
+        model = svm_train (prob, param)
+        
+        y = []
+        x = []
+        for item in test:
+            #y.append (name2label (item['attr']['Disease'], label_name))
+            y.append (label2int (item['attr']['Disease']))
+            x.append (item['pos'])
+
+        pred = svm_predict (y, x, model, '-q')
+
+        #if int (pred[0][0]) == name2label (test[0]['attr']['Disease'], label_name):
+        test_label = test[0]['attr']['Disease']
+        if int (pred[0][0]) == label2int (test_label):
+            correct[test_label] += 1
+        else:
+            print "Misclassified: %s as %s in row %d" % (test[0]['attr']['Disease'], int2label (pred[0][0]), i)
+            wrong[test_label] += 1
+        
+        #print test[0]['attr']['Disease'] + ' ' + str (name2label (test[0]['attr']['Disease'], label_name))  + ' ' + str (pred[0][0])
+
+    print ''
+
+    total_correct = 0
+    total_wrong = 0
+
+    for label in labels:
+        print "%s identified %d/%d times (%d percent)" % (label, correct[label], correct[label] + wrong[label], int (round (float (correct[label]) / float (correct[label] + wrong[label]) * 100.00)))
+        total_correct += correct[label]
+        total_wrong += wrong[label]
+
+    print ''
+    print "Total Correct: %d/%d" % (total_correct, total_correct + total_wrong)
---
+++ svm/io.py
@@ -0,0 +1,69 @@
+import csv
+
+import networkx as nx
+from networkx.readwrite import json_graph
+
+Y = 100
+MINOR = 25
+
+def read_table (path):
+    nodes = []
+    #buffer = open (path, 'r').read ()
+    #buffer = buffer.replace ('\r', '')
+    #rows = buffer.split ('\n')
+    rows = csv.reader (open (path, 'rU'))
+    contrib = []
+    for elem in rows.next ():
+        if len (elem) > 0:
+            contrib.append (True)
+        else:
+            contrib.append (False)
+    keys = rows.next ()
+    id = 0
+    for row in iter (rows):
+        #if len (row) == 0:
+        #    continue
+        pos = []
+        attr = {}
+        for key, value, include in zip (keys, row, contrib):
+            #print ' '.join ([key, value, str (include)])
+            if include:
+                if value == 'y':
+                    value = Y
+                elif value == 'minor':
+                    value = MINOR
+                elif len (value) == 0:
+                    value = 0
+                pos.append (float (value))
+            attr[key] = value
+        item = {
+            '_id': id,
+            'pos': pos,
+            'attr': attr
+            }
+        nodes.append (item)
+        id += 1
+    return nodes
+
+
+def equal_weights (nodes):
+    weights = []
+    for i in range (0, len (nodes[0]['pos'])):
+        weights.append (1.0)
+    return weights
+
+
+def make_graph (nodes, weights):
+    G = nx.Graph ()
+    for node in nodes:
+        G.add_node (node['_id'], node)
+    for i, first in enumerate (nodes):
+        for j, second in enumerate (nodes):
+            if j >= i:
+                continue
+            total = 0.0
+            for w, f, s in zip (weights, first['pos'], second['pos']):
+                total += w * ((f - s) / 100.0)
+            if total > 0.0:
+                G.add_edge (first['_id'], second['_id'], {'weight': total})
+    return G
