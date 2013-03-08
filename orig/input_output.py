48e78293e971625bde9d9ccd713ac3fcb5ce68b8
mgibson <gusgibson@yahoo.com>
2013-03-08 13:44:59 -0500

    made io a class needed to record length of vector as gone now with dict vector is now a dict since its sparse

--- orig/input_output.py
+++ orig/input_output.py
@@ -3,64 +3,51 @@ import csv
 import networkx as nx
 from networkx.readwrite import json_graph
 
-Y = 100
-MINOR = 25
+import pdb
 
-def read_table (path):
-    nodes = []
-    rows = csv.reader (open (path, 'r'))
-    contrib = []
-    # The first row contains a 1 or NULL that indicates if the field below contributes to the vector
-    for elem in rows.next ():
-        if len (elem) > 0:
-            contrib.append (True)
-        else:
-            contrib.append (False)
-    # The second row contains the headers
-    keys = rows.next ()
-    id = 0
-    for row in iter (rows):
-        pos = []
-        attr = {}
-        for key, value, include in zip (keys, row, contrib):
-            # Cleanup the strings from the data
-            if include:
-                if value == 'y':
-                    value = Y
-                elif value == 'minor':
-                    value = MINOR
-                elif len (value) == 0:
-                    value = 0
-                pos.append (float (value))
-            attr[key] = value
-        item = {
-            '_id': id,
-            'pos': pos,
-            'attr': attr
-            }
-        nodes.append (item)
-        id += 1
-    return nodes
+class IO:
 
+    Y = 100
+    MINOR = 25
 
-def equal_weights (nodes):
-    weights = []
-    for i in range (0, len (nodes[0]['pos'])):
-        weights.append (1.0)
-    return weights
+    def read_table (self,path):
+        self.vectorLength = 0
+        nodes = []
+        rows = csv.reader (open (path, 'r'))
+        contrib = []
+        # The first row contains a 1 or NULL that indicates if the field below contributes to the vector
+        for elem in rows.next ():
+            if len (elem) > 0:
+                contrib.append (True)
+                self.vectorLength += 1
+            else:
+                contrib.append (False)
 
-
-def make_graph (nodes, weights):
-    G = nx.Graph ()
-    for node in nodes:
-        G.add_node (node['_id'], node)
-    for i, first in enumerate (nodes):
-        for j, second in enumerate (nodes):
-            if j >= i:
-                continue
-            total = 0.0
-            for w, f, s in zip (weights, first['pos'], second['pos']):
-                total += w * ((f - s) / 100.0)
-            if total > 0.0:
-                G.add_edge (first['_id'], second['_id'], {'weight': total})
-    return G
+        # The second row contains the headers
+        keys = rows.next ()
+        id = 0
+        for row in iter (rows):
+            # pos is the position vector in zack speak
+            # its a hash because its sparse, not recording empty values
+            pos = {}
+            attr = {}
+            i = 0
+            for key, value, include in zip (keys, row, contrib):
+                # Cleanup the strings from the data
+                if include and len(value) > 0:
+                    if value == 'y':
+                        value = IO.Y
+                    elif value == 'minor':
+                        value = IO.MINOR
+                    pos[i] = (float(value))
+                    if include:
+                        i += 1
+                attr[key] = value
+            item = {
+                '_id': id,
+                'pos': pos,
+                'attr': attr
+                }
+            nodes.append (item)
+            id += 1
+        return nodes
