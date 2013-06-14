73af40b55d5acd43103f2304d593e8274850af74
Amy Slagle <slagle@ehatek.org>
2013-06-14 11:32:27 -0400

    Rename src directory to bratwurst.

--- ner/bratwurst/config.py
+++ ner/bratwurst/config.py
@@ -1,4 +1,4 @@
 BRAT_DATA_LOCATION = "../brat-v1.3_Crunchy_Frog/data/promed/"
 BRAT_URL = "http://localhost:8001/index.xhtml#/promed/"
-LIB_PATH = "src/lib"
+LIB_PATH = "bratwurst/lib"
 CLASSIFIER_PATH = "classifiers"
